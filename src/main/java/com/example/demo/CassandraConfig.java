3759b39a09735b52bd49a9315d0ae655d31b8f4e
Angeli <angeli.corral@bestbuy.com>
2017-11-20 23:59:32 -0600

    Other files

---
+++ src/main/java/com/example/demo/CassandraConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
+import org.springframework.data.cassandra.config.java.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.mapping.BasicCassandraMappingContext;
+import org.springframework.data.cassandra.mapping.CassandraMappingContext;
+
+@Configuration
+public class CassandraConfig extends AbstractCassandraConfiguration {
+ 
+    @Override
+    protected String getKeyspaceName() {
+        return "das";
+    }
+ 
+    @Bean
+    public CassandraClusterFactoryBean cluster() {
+        CassandraClusterFactoryBean cluster = 
+          new CassandraClusterFactoryBean();
+        cluster.setContactPoints("localhost");
+        cluster.setPort(9042);
+        cluster.setUsername("cassandra");
+        cluster.setPassword("cassandra");
+        return cluster;
+    }
+ 
+    @Bean
+    public CassandraMappingContext cassandraMapping() 
+      throws ClassNotFoundException {
+        return new BasicCassandraMappingContext();
+    }
+}
---
+++ src/main/java/com/example/demo/CustomerRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.data.cassandra.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+ 
+ 
+public interface CustomerRepository extends CrudRepository<Customer, String> {
+	
+	@Query(value="SELECT * FROM customer WHERE firstname=?0")
+	public List<Customer> findByFirstname(String firstname);
+ 
+	@Query("SELECT * FROM customer WHERE age > ?0")
+	public List<Customer> findCustomerHasAgeGreaterThan(int age);
+}	
